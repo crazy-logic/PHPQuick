f680157f2d9b2f7a0a9b68bf45070b2041204128
Matt <matt@crazy-logic.co.uk>
2023-01-25 11:55:16 +0000

    Update PHPQuickAnalytics.php

--- PHPQuickAnalytics.php
+++ PHPQuickAnalytics.php
@@ -5,6 +5,7 @@
 // include "PHPQuickAnalytics.php";
 // call analytics_record($conn); to record a view - $conn being the database. 
 // call analytics_session($conn); for a view of the analytics. 
+// call analytics_report($conn); for a day's worth of aanalytics - this is returned rather than echoed as it's designed to be used as a scheduled mail thing. 
 // no escaping so not safe for production. 
 
 //if the table doesn't exist it creates it. $conn must exist before this is included.
@@ -156,6 +157,107 @@ function analytics_sessions($conn)
 	
 }
 
+function analytics_report($conn) //this returns rather than echos 
+{
+	$reply = "";
+	//$date = "2023-01-25 00:00:00";
+	//mysql datetime format 
+	$date = date("Y-m-d H:i:s", (time()-(24*60*60)));
+	
+	
+	
+	$reply .= "<h2>Unique users report</h2>";
+	
+	$sql = "SELECT CONCAT(address,'_',agent,'_',session_id) AS CC, session_id FROM `Analytics` WHERE `date` > '$date'  GROUP BY `session_id`;";
+	$result = mysqli_query($conn, $sql);
 
+	//lets get the feild names
+	$fields = [];
+	foreach(mysqli_fetch_fields($result) as $column){array_push($fields,$column->name);}
+
+	//lets echo the table header. 
+	$reply .= "<table border=0><tr>";
+	foreach ($fields as $feild) {$reply .= "<th>".$feild."</th>";}	
+	$reply .= "</tr>";
+	
+	//and now fill it with the data. 
+	if (mysqli_num_rows($result) > 0) 
+	{
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+    		$reply .= "<tr>";
+    		foreach ($fields as $field)
+        	{$reply .= "<td>".$row[$field]."</td>";}
+    		$reply .= "</tr>";
+	 	}
+	} 
+	else 
+	{$reply .= "<tr><td colspan=".sizeof($fields)." align='center'> No results found. </td></tr>";}
+	$reply .= "</table>";
+	
+	
+	//SELECT [activity_dt], count(*) FROM table1 GROUP BY hour( activity_dt ) 
+	
+	$reply .= "<h2>By hour</h2>";
+	
+	$sql = "SELECT count(*) AS views, hour(`date`) AS Hour FROM `Analytics` WHERE `date` > '$date' GROUP BY hour(`date`);";
+	$result = mysqli_query($conn, $sql);
+
+	//lets get the feild names
+	$fields = [];
+	foreach(mysqli_fetch_fields($result) as $column){array_push($fields,$column->name);}
+
+	//lets echo the table header. 
+	$reply .= "<table border=0><tr>";
+	foreach ($fields as $feild) {$reply .= "<th>".$feild."</th>";}	
+	$reply .= "</tr>";
+	
+	//and now fill it with the data. 
+	if (mysqli_num_rows($result) > 0) 
+	{
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+    		$reply .= "<tr>";
+    		foreach ($fields as $field)
+        	{$reply .= "<td>".$row[$field]."</td>";}
+    		$reply .= "</tr>";
+	 	}
+	} 
+	else 
+	{$reply .= "<tr><td colspan=".sizeof($fields)." align='center'> No results found. </td></tr>";}
+	$reply .= "</table>";
+	
+	
+	$reply .= "<h2>By pages</h2>";
+	
+	$sql = "SELECT COUNT(uri) AS Views, uri AS Page FROM `Analytics` WHERE `date` > '$date' GROUP BY uri;";
+	$result = mysqli_query($conn, $sql);
+
+	//lets get the feild names
+	$fields = [];
+	foreach(mysqli_fetch_fields($result) as $column){array_push($fields,$column->name);}
+
+	//lets echo the table header. 
+	$reply .= "<table border=0><tr>";
+	foreach ($fields as $feild) {$reply .= "<th>".$feild."</th>";}	
+	$reply .= "</tr>";
+	
+	//and now fill it with the data. 
+	if (mysqli_num_rows($result) > 0) 
+	{
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+    		$reply .= "<tr>";
+    		foreach ($fields as $field)
+        	{$reply .= "<td>".$row[$field]."</td>";}
+    		$reply .= "</tr>";
+	 	}
+	} 
+	else 
+	{$reply .= "<tr><td colspan=".sizeof($fields)." align='center'> No results found. </td></tr>";}
+	$reply .= "</table>";
+	
+	return $reply;
+}
 
 ?>
