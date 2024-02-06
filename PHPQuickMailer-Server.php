9836619867764afeacbb3710c07ed8bcf5855ac3
Matt <matt@crazy-logic.co.uk>
2024-02-06 13:47:48 +0000

    Adding secret and also client allowed list

--- PHPQuickMailer-Server.php
+++ PHPQuickMailer-Server.php
@@ -1,4 +1,8 @@
 <?php
+//script recieves a json variable for the mail infomation 
+$secret = '395f4f5539da5154b44884848d990992';
+$allowedAddresses = array('1.1.1.1','127.0.0.1','8.8.8.8');
+
 
 function mailmessage($message, $to, $subject, $from)
 {
@@ -13,26 +17,36 @@ function mailmessage($message, $to, $subject, $from)
 	mail($to, $subject, $message, $headers);
 }
 
+function validclient($allowedAddresses,$address)
+{
+	if(in_array($address,$allowedAddresses))
+	{return true;}
+	else
+	{return false;}
+}
 
-if(isset($_POST['json']))
+//if behind a proxy you may need to change this to 
+// $_SERVER['HTTP_CLIENT_IP'] or $_SERVER['HTTP_X_FORWARDED_FOR']
+if(isset($_POST['json']) && validclient($allowedAddresses,$_SERVER['REMOTE_ADDR']))
 {
     $array = json_decode($_POST['json'],true);
-    $to = $array['to'];
-    $from = $array['from'];
-    $subject = $array['subject'];
-    $message = $array['message'];
-    mailmessage($message, $to, $subject, $from);
-    echo "mail sent";
-    
-    //echo "</br>";
-    //echo "$to</br>";
-    //echo "$from</br>";
-    //echo "$subject</br>";
-    //echo "$message</br>";
+    if(isset($array['secret']) && $array['secret']===$secret)
+    {
+    	$to = $array['to'];
+	    $from = $array['from'];
+	    $subject = $array['subject'];
+	    $message = $array['message'];
+	    mailmessage($message, $to, $subject, $from);
+	    echo "mail sent";
+    }
+    else
+    {
+    	echo "secret not correct. ";
+    }
 }
 else
 {
-    echo "no data passed to mail.php";
+    echo "no data passed to mail.php or not a valid client: ".$_SERVER['REMOTE_ADDR'];
 }
 
 ?>
