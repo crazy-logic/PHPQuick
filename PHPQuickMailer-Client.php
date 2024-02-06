d9fd6abf8cfb37798c943988428076d5c9bc9e64
Matt <matt@crazy-logic.co.uk>
2024-02-06 13:50:02 +0000

    Adding shared secret

--- PHPQuickMailer-Client.php
+++ PHPQuickMailer-Client.php
@@ -1,14 +1,24 @@
 <?php
+// script sends a json variable for the mail infomation 
+//this script required the php-curl to be installed. 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$remoteAddress = "https://domain_of_server/Path/To/File/PHPQuickMailer-Server.php";
+$secret = '395f4f5539da5154b44884848d990992';
+
 
 //array for the stuff to send
 $array['message'] = "Hi this is some <b>HMTL</b>";
 $array['subject'] = "Testing email";
-$array['to'] = "";
-$array['from'] = "";
+$array['to'] = "user@domain";
+$array['from'] = "user@domain";
+$array['secret'] = $secret;
 
 $json = "json=".json_encode($array);
 
-$ch = curl_init('https://pathtoremoteserver/mailer.php');
+$ch = curl_init($remoteAddress);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
